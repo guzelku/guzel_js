6c44829d6b0d65eb5749a2184e6d78424f7560b7
guzelku <62844142+guzelku@users.noreply.github.com>
2020-03-30 10:06:23 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-let money=1; 
-let income=2; 
-let addExpenses=3; 
-let deposit=4; 
-let mission='text'; 
-let period=6;
-alert('изменение');
-console.log('всё получится');
+let money=50000; 
+let income=0; 
+let addExpenses='интернет, такси, коммуналка'; 
+let deposit=true; 
+let mission=1500000; 
+let period=12;
+
+let  budgetDay;
+budgetDay=money/30;
+
+
+
+
+console.log(typeof(money));
+console.log(typeof(income));
+console.log(typeof(deposit));
+console.log(addExpenses.length);
+console.log('Период равен'+' '+ period +' ' +'месяцев');
+console.log('Цель заработать'+' '+ mission +' ' +'рублей');
+console.log(budgetDay);
+console.log(addExpenses.length);
+console.log(addExpenses.toLocaleLowerCase());
+console.log(addExpenses.split(', '));

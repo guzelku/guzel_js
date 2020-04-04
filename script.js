417dfc7e80da74d47b32d628e8415694f0020266
guzelku <62844142+guzelku@users.noreply.github.com>
2020-04-05 00:46:05 +0500

    Update script.js

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ console.log(addExpenses.toLowerCase().split(','));
 let start = function(){
 do
 {
-money=prompt('Ваш месячный доход?');
+money=+prompt('Ваш месячный доход?');
 }
 while (!isNumber( money));
 }

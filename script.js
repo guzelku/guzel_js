405a451ce1d4767c4c52dfe5e56004e483224821
guzelku <62844142+guzelku@users.noreply.github.com>
2020-04-07 00:11:37 +0500

    Update script.js

--- script.js
+++ script.js
@@ -26,46 +26,58 @@ showTypeOf (deposit);
 addExpenses=prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
 console.log(addExpenses.toLowerCase().split(','));
 
+
 let start = function(){
 do
 {
 money=+prompt('Ваш месячный доход?');
 }
-while (!isNumber( money));
+while (!isNumber(money));
 }
 
 start();
 
-let expenses=[];
 
+
+
+
+let expenses=[];
+/**Функция возвращает сумму всех обязательных */
 let getExpensesMonth = function(){
 
 let sum = 0;
 
-for(let i=0; i<2; i++){
-    expenses[i] = prompt('Введите обязательнуб статью расходов');
-   
-  let  x=prompt('Во сколько это обойдется?');
-  while (!isNumber(x))
-  {
-  x= prompt('Во сколько это обойдется?')
-  }
-   sum+=x;
-   
+for(let i=0; i<3; i++){
+expenses[i] = prompt('Введите обязательнуб статью расходов');
+
+let x=+prompt('Во сколько это обойдется?');
+
+while (!isNumber(x))
+{
+x=+prompt('Во сколько это обойдется?');
+}
+sum+=x;
+
 }console.log(expenses);
-    return sum;
+return sum;
 }
 
 
-
+/**переменная = сумму всех обязательных */
 let expensesAmount =getExpensesMonth();
+
 
 
 let getAccumulatedMonth =function (){
 return money-expensesAmount;
 }
+
+
 let accumulatedMonth=getAccumulatedMonth();
 console.log('Накопления за месяц:',accumulatedMonth);
+
+/*Если getTargetMonth возвращает нам отрицательное значение,
+то вместо “Цель будет достигнута” необходимо выводить “Цель не будет достигнута” */
 
 let getTargetMonth =function (){
 if (Math.ceil (mission/accumulatedMonth) <0) {console.log('цель не будет достигнута') ; }
@@ -74,22 +86,3 @@ console.log('цель будет достигнута за'+' '+Math.ceil (missi
 }
 }
 getTargetMonth();
-
-
-
-budgetDay = accumulatedMonth/30;
-console.log('бюджет на день '+' '+Math.floor(budgetDay));
-
-let getStatusIncome = function() {
-if(budgetDay>1200){return ('У вас высокий уровень дохода');}
-else
-if(budgetDay>600 && budgetDay<=1200)
-{return ('У вас средний уровень дохода');}
-else
-if(budgetDay>0 && budgetDay<=600)
-{return ('К сожалению у вас уровень дохода ниже среднего');}
-else
-{return ('Что то пошло не так');}
-}
-
-console.log(getStatusIncome());

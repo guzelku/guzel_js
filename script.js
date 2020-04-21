d8389e0abca4620120c4cc9fd3cd09232d66f6ff
guzelku <62844142+guzelku@users.noreply.github.com>
2020-04-21 22:47:31 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,39 @@
-let money=1; 
-let income=2; 
-let addExpenses=3; 
-let deposit=4; 
-let mission='text'; 
-let period=6;
-alert('изменение');
-console.log('всё получится');
+//Напишите расширения метода прототипа:
+
+//1) Два класса, First и Second, Second наследует от First .
+
+//2) В First есть метод hello - он печатает в консоль "Привет я метод родителя!".
+
+//3) Нужно написать в Second метод hello, чтоб он сначала вызывал метод hello из First,
+// а потом сразу печатал в консоль "А я наследуемый метод!"*/
+
+
+
+ class First {
+   constructor(name) {
+      this.name = name;
+      this.phrase= `Привет я метод родителя!`;
+    }
+    hello() {
+ 
+      console.log(`${this.phrase}`);
+      return this.phrase
+   }
+ }
+
+ 
+
+ //прототипное наследование
+
+ class Second extends First {
+      hello(phrase) {
+     console.log(`${super.hello(phrase)} 
+А я наследуемый метод! `);
+   }
+ }
+
+ const se = new Second();
+se.hello(); 
+
+
+
